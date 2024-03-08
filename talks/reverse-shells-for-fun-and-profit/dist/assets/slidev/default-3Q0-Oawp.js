406df32e62f34345f761da5784b94b780cd0d4fb
Jeff Martin <nodeselector@github.com>
2024-03-07 19:36:47 -0800

    new deploy idea

---
+++ talks/reverse-shells-for-fun-and-profit/dist/assets/slidev/default-3Q0-Oawp.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue--2rrS9jf.js";import{b as l}from"../index-BQcGyKHg.js";const c={},d={class:"slidev-layout default"};function n(e,r){return o(),t("div",d,[s(e.$slots,"default")])}const _=l(c,[["render",n],["__file","/opt/homebrew/lib/node_modules/@slidev/cli/node_modules/@slidev/client/layouts/default.vue"]]);export{_ as I};
